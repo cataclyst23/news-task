ac19207f86cccc7c6b2a4eda86eabfdcc730ad9e
Csongor Bartha <csongor.bartha@gmail.com>
2023-11-05 03:43:57 +0100

    fmt prints changed to use the log package

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -2,8 +2,8 @@ package client
 
 import (
 	"encoding/json"
-	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"net/url"
 
@@ -33,10 +33,10 @@ func ResponseHandler(w http.ResponseWriter, r *http.Request) {
 	responseObject := &utils.ResponseJson{}
 
 	if err := json.Unmarshal(makeRequest(), responseObject); err != nil {
-		fmt.Printf("Error occured during JSON marshaling: %s\n", err.Error())
+		log.Printf("Error occured during JSON marshaling: %s\n", err.Error())
 	}
 
-	fmt.Println(utils.PrettyPrint(responseObject))
+	log.Println(utils.PrettyPrint(responseObject))
 
 	p := &utils.Page{
 		Title: utils.PageTitle,
@@ -60,16 +60,16 @@ func makeRequest() []byte {
 
 	req, err := http.NewRequest(http.MethodGet, url.String(), nil)
 	if err != nil {
-		fmt.Printf("Error building the HTTP request: %s\n", err.Error())
+		log.Printf("Error building the HTTP request: %s\n", err.Error())
 	}
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		fmt.Printf("Error making the HTTP request: %s", err.Error())
+		log.Printf("Error making the HTTP request: %s", err.Error())
 	}
 
 	if resp.StatusCode <= 200 && resp.StatusCode < 400 {
-		fmt.Printf("Response code: %d\n", resp.StatusCode)
+		log.Printf("Response code: %d\n", resp.StatusCode)
 
 	}
 
@@ -77,7 +77,7 @@ func makeRequest() []byte {
 
 	data, err := io.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Printf("Could not read response body: %s\n", err)
+		log.Printf("Could not read response body: %s\n", err)
 	}
 
 	return data
